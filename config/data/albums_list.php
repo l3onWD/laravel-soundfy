02ce8756a08e9276f2ba901519f320d6d67f6ae6
l3on <l3onwebdev@gmail.com>
2023-10-22 19:32:03 +0200

    added fake album covers

--- config/data/albums_list.php
+++ config/data/albums_list.php
@@ -1,33 +1,33 @@
-<?php 
+<?php
 return [
     [
         'author_id' => 1,
         'title' => 'Jökull',
-        'cover' => null,
+        'cover' => 'https://picsum.photos/id/443/300/300',
         'release_date' => 20180126
     ],
     [
         'author_id' => 1,
         'title' => 'Unreleased Jewels (vol.1)',
-        'cover' => null,
+        'cover' => 'https://picsum.photos/id/83/300/300',
         'release_date' => 202200804
     ],
     [
         'author_id' => 1,
         'title' => 'Unreleased Jewels (vol.2)',
-        'cover' => null,
+        'cover' => 'https://picsum.photos/id/343/300/300',
         'release_date' => 202200804
     ],
     [
         'author_id' => 1,
         'title' => 'Maximus Melodius Harmonicus',
-        'cover' => null,
+        'cover' => 'https://picsum.photos/id/209/300/300',
         'release_date' => 20230117
     ],
     [
         'author_id' => 2,
         'title' => 'Metal Gear',
-        'cover' => null,
+        'cover' => 'https://picsum.photos/id/137/300/300',
         'release_date' => 20230101
     ]
 ];
