a46c29da44f76692017b44f46bde4739af7a15fb
l3on <l3onwebdev@gmail.com>
2023-10-22 20:05:03 +0200

    fixed tracks src

--- config/data/tracks_list.php
+++ config/data/tracks_list.php
@@ -4,36 +4,36 @@ return [
         'title' => 'Might & Magic',
         'album_id' => 3,
         'duration' => 357,
-        'src' => '/storage/tracks/might-amp-magic-138999.mp3',
+        'src' => '/tracks/might-amp-magic-138999.mp3',
     ],
     [
         'title' => 'Metal Gear',
         'album_id' => 5,
         'duration' => 100,
-        'src' => '/storage/tracks/metal-gear-136567.mp3',
+        'src' => '/tracks/metal-gear-136567.mp3',
     ],
     [
         'title' => 'Woods of Imagination',
         'album_id' => 4,
         'duration' => 213,
-        'src' => '/storage/tracks/woods-of-imagination-139004.mp3',
+        'src' => '/tracks/woods-of-imagination-139004.mp3',
     ],
     [
         'title' => 'Lord of the Riffs',
         'album_id' => 2,
         'duration' => 387,
-        'src' => '/storage/tracks/lord-of-the-riffs-138993.mp3',
+        'src' => '/tracks/lord-of-the-riffs-138993.mp3',
     ],
     [
         'title' => 'The Wizard',
         'album_id' => 4,
         'duration' => 247,
-        'src' => '/storage/tracks/the-wizard-139003.mp3',
+        'src' => '/tracks/the-wizard-139003.mp3',
     ],
     [
         'title' => 'Jökull (Metal Version)',
         'album_id' => 1,
         'duration' => 245,
-        'src' => '/storage/tracks/jokull-metal-version-138992.mp3',
+        'src' => '/tracks/jokull-metal-version-138992.mp3',
     ]
 ];
