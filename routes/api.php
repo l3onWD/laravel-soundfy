43d6a5510b25ad1f86c039886ce0e7366468770e
l3on <l3onwebdev@gmail.com>
2023-10-29 18:40:38 +0100

    changed api stream logic

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Track;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 /*
@@ -24,12 +25,24 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 // Track Routes
 Route::get('/tracks/{track}/stream', function (Track $track) {
 
-    // Create a bynary stream
-    $response = new BinaryFileResponse(storage_path() . $track->src);
-    BinaryFileResponse::trustXSendfileTypeHeader();
+    // Get File Data
+    $file_name = $track->file_name;
+    $file_path = storage_path('tracks/' . $file_name);
+    $file_content = file_get_contents($file_path);
+    $file_size = filesize($file_path);
 
-    // Send response
-    return $response;
+    // Set mime type
+    $mime_type = "audio/mpeg";
+
+    // Set Headers
+    $headers = [
+        'Accept-Ranges: 0-' . ($file_size - 1),
+        'Content-Length:' . $file_size,
+        'Content-Type:' . $mime_type,
+        'Content-Disposition: inline; filename="' . $file_name . '"'
+    ];
+
+    return response($file_content, 200, $headers);
 });
 
 Route::get('/tracks', function () {
