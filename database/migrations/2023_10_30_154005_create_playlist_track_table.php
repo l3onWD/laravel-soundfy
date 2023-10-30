40c07ae60b5e2012683914deaa136eca7d1622e0
l3on <l3onwebdev@gmail.com>
2023-10-30 15:53:52 +0100

    added playlist track pivot table and seeder

---
+++ database/migrations/2023_10_30_154005_create_playlist_track_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('playlist_track', function (Blueprint $table) {
+            $table->foreignId('playlist_id')->constrained()->onDelete('cascade');
+            $table->foreignId('track_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('playlist_track');
+    }
+};
--- database/seeders/PlaylistSeeder.php
+++ database/seeders/PlaylistSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Playlist;
+use App\Models\Track;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class PlaylistSeeder extends Seeder
 {
@@ -16,6 +18,9 @@ class PlaylistSeeder extends Seeder
         // Get system playlists
         $playlists = config('data.playlists');
 
+        // Get all initial tracks
+        $track_ids = Track::pluck('id')->toArray();
+
         foreach ($playlists as $playlist) {
 
             $new_playlist = new Playlist();
@@ -25,6 +30,18 @@ class PlaylistSeeder extends Seeder
             $new_playlist->cover = $playlist['cover'];
 
             $new_playlist->save();
+
+
+            // Add random tracks
+            $playlist_tracks = [];
+            foreach ($track_ids as $track_id) {
+                if (rand(0, 1)) $playlist_tracks[] = $track_id;
+            }
+
+            // Add at least 1 track
+            if (empty($playlist_tracks)) Arr::random($track_ids);
+
+            $new_playlist->tracks()->attach($playlist_tracks);
         }
     }
 }
