2c9e9d429a336511227ddb96ec1bd7a735ea2beb
l3on <l3onwebdev@gmail.com>
2023-10-30 18:59:08 +0100

    fixed playlist name

--- config/data/playlists.php
+++ config/data/playlists.php
@@ -8,11 +8,11 @@ return [
     [
         'user_id' => 1,
         'title' => 'Knights and Swords',
-        'cover' => 'https://picsum.photos/id/83/300/300'
+        'cover' => 'https://picsum.photos/id/343/300/300'
     ],
     [
         'user_id' => 1,
-        'title' => 'Into the wood',
-        'cover' => 'https://picsum.photos/id/343/300/300'
+        'title' => 'Into the Woods',
+        'cover' => 'https://picsum.photos/id/83/300/300'
     ]
 ];
