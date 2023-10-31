abbff58b87035339e8d012fae8594ee46af5737d
l3on <l3onwebdev@gmail.com>
2023-10-31 18:47:42 +0100

    changed playlist covers

--- config/data/playlists.php
+++ config/data/playlists.php
@@ -3,16 +3,16 @@ return [
     [
         'user_id' => 1,
         'title' => 'Metal Contagion',
-        'cover' => 'https://picsum.photos/id/443/300/300'
+        'cover' => 'https://picsum.photos/id/145/300/300'
     ],
     [
         'user_id' => 1,
         'title' => 'Knights and Swords',
-        'cover' => 'https://picsum.photos/id/343/300/300'
+        'cover' => 'https://picsum.photos/id/353/300/300'
     ],
     [
         'user_id' => 1,
         'title' => 'Into the Woods',
-        'cover' => 'https://picsum.photos/id/83/300/300'
+        'cover' => 'https://picsum.photos/id/229/300/300'
     ]
 ];
