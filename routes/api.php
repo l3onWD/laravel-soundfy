cc7f581dc6d52a6e7b30a509bc35d171087d5ac4
l3on <l3onwebdev@gmail.com>
2023-11-01 11:56:46 +0100

    deleted unused endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use App\Http\Controllers\api\AlbumController;
 use App\Http\Controllers\api\PlaylistController;
 use App\Http\Controllers\api\TrackController;
-use App\Models\Album;
-use App\Models\Playlist;
-use App\Models\Track;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,21 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Auth routes
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-// Track Routes
-Route::get('/tracks/{track}/stream', [TrackController::class, 'stream']);
-Route::get('/tracks/random', [TrackController::class, 'random']);
-Route::get('/tracks/{track}', [TrackController::class, 'show']);
-
-Route::get('/tracks', function () {
-
-    $tracks = Track::select('id', 'album_id', 'title', 'duration')->with(['album'])->get();
-
-    return response()->json($tracks);
 });
 
 
@@ -46,3 +31,9 @@ Route::get('/playlists/{playlist}', [PlaylistController::class, 'show']);
 // Album Routes
 Route::get('/albums/random', [AlbumController::class, 'random']);
 Route::get('/albums/{album}', [AlbumController::class, 'show']);
+
+
+// Track Routes
+Route::get('/tracks/{track}/stream', [TrackController::class, 'stream']);
+Route::get('/tracks/random', [TrackController::class, 'random']);
+Route::get('/tracks/{track}', [TrackController::class, 'show']);
